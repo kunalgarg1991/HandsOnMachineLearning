e2f735e744ef08a3f476f88685faebf8c5229951
Kunal Garg <kunalgarg@Kunals-MacBook-Air.local>
2022-01-07 01:21:49 +0530

    chapter2

--- chaptwo.py
+++ chaptwo.py
@@ -1,27 +1,330 @@
 import pandas as pd
+import numpy as np
+
 housing = pd.read_csv("housing.csv")
+
 print (housing.head())
 print (housing.info())
-print (housing["ocean_proximity"].value_counts())
 print(housing.describe())
+print (housing["ocean_proximity"].value_counts())
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+print(housing["income_cat"].value_counts())
+housing["income_cat"].hist()
+
+"""Discover and Visualize the data to gain insights"""
 
 import matplotlib.pyplot as plt
 housing.hist(bins = 50, figsize=(20,15))
 plt.savefig("attributes.png", format = "png")
 plt.show()
 
-import numpy as np
-from sklearn.model_selection import train_test_split
-train_Set, test_set= train_test_split(housing, test_size=0.2, random_state = 43)
-print(test_set.head(5))
-
 housing["median_income"].hist()
 plt.show()
 
-housing["income_cat"] = pd.cut(housing["median_income"],
-                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
-                               labels=[1, 2, 3, 4, 5])
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha = 0.1)
+plt.show()
 
-print(housing["income_cat"].value_counts())
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha = 0.1, s=housing["population"]/100, label="population", figsize=(10,7),
+             c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+             sharex=False)
+plt.legend()
+plt.show()
 
-housing["income_cat"].hist()
+"""Looking for correlations"""
+corr_matrix = housing.corr()
+print(corr_matrix["median_house_value"])
+
+"""from pandas.plotting import scatter_matrix
+attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12.8))
+plt.show()
+"""
+
+"""Experimenting with attribute combinations"""
+
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+
+corr_matrix = housing.corr()
+print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+housing.plot(kind="scatter", x="rooms_per_household", y="median_house_value",
+             alpha=0.2)
+plt.axis([0, 5, 0, 520000])
+plt.show()
+print(housing.describe())
+
+"""Splitting training and test set"""
+
+from sklearn.model_selection import train_test_split
+train_set, test_set= train_test_split(housing, test_size=0.2, random_state = 43)
+print(test_set.head(5))
+
+"""Seperating the labels and features"""
+
+housing = train_set.drop("median_house_value", axis=1) # drop labels for training set
+housing_labels = train_set["median_house_value"].copy()
+
+
+"""Preparing the data for machine learning - 
+
+3 transformations on the numerical data - removing null values, adding extra attributes, scaling the values
+1 transformation on categorical data - onehotencoder to convert categorical data into matrix of 0 and 1
+
+Separating the data into numerical and textual values"""
+
+housing_num = housing.drop("ocean_proximity", axis=1)
+housing_cat = housing[["ocean_proximity"]]
+
+
+"""Converting categorical data into array
+1. Ordinal Encoder - assigns a number to each value of the categorical value starting from 0
+2. One hot encoder: converts into sparse matrix - This is better because the ordinal encoder values are 
+considered ordered 0<1<!<2,etc"""
+
+from sklearn.preprocessing import OrdinalEncoder
+ordinalencoder = OrdinalEncoder()
+housing_cat_encoded=ordinalencoder.fit_transform(housing_cat)
+print(housing_cat_encoded[:10])
+print(ordinalencoder.categories_)
+
+from sklearn.preprocessing import OneHotEncoder
+onehotencoder=OneHotEncoder()
+housing_cat_oneencoded = onehotencoder.fit_transform(housing_cat)
+print(housing_cat_oneencoded)
+print(housing_cat_oneencoded.toarray())
+
+
+
+"""
+1. Cleaning the numerical data - removing null values  There are 3 ways of doing it
+housing.dropna(subset=["total_bedrooms"])    # option 1 - removing the rows
+housing.drop("total_bedrooms", axis=1)       # option 2 - removing the column altogether
+median = housing["total_bedrooms"].median()  # option 3 - replacing the blank value with some other value 
+housing["total_bedrooms"].fillna(median, inplace=True)
+
+The transformer simpleimputer can be used also for it
+
+"""
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy="median")
+imputer.fit(housing_num)
+print(imputer.statistics_)
+"""above is also eqaul in values to the median of the columns calculated by the following"""
+print(housing_num.median().values)
+X=imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                          index=housing.index)
+print(imputer.strategy)
+
+
+"""Cleaning the numerical data  - adding custom attributes"""
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# column index
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+"""netter to get these values dynamically as
+col_names = "total_rooms", "total_bedrooms", "population", "households"
+rooms_ix, bedrooms_ix, population_ix, households_ix = [
+    housing.columns.get_loc(c) for c in col_names]"""
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+    def transform(self, X):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+"""Also, housing_extra_attribs is a NumPy array, we've lost the column names (unfortunately, that's a 
+problem with Scikit-Learn). To recover a DataFrame, you could run this:"""
+
+housing_extra_attribs = pd.DataFrame(
+    housing_extra_attribs,
+    columns=list(housing.columns)+["rooms_per_household", "population_per_household"],
+    index=housing.index)
+print(housing_extra_attribs.head())
+
+"""transformation -3 scaling the numberical data
+
+2 classes can be used to do it - MinMaxScalar and StandardScaler()"""
+
+from sklearn.preprocessing import StandardScaler
+stdscalar = StandardScaler()
+housing_num_scaled = stdscalar.fit_transform(housing_num)
+
+"""Doing all the transformations for numerical data in one pipeline"""
+from sklearn.pipeline import Pipeline
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+"""Doing all transformations for categorical data in one pipeline"""
+cat_pipeline = Pipeline([
+        ("onehot", OneHotEncoder())
+    ])
+housing_cat_tr = cat_pipeline.fit_transform(housing_cat)
+
+"""Column transformer can be used to combine all this work"""
+from sklearn.compose import ColumnTransformer
+
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", OneHotEncoder(), cat_attribs),
+    ])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+"""note that housing_prepared is an array"""
+
+"""Selecting and training a model"""
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+housing_predictions = lin_reg.predict(housing_prepared)
+
+from sklearn.metrics import mean_squared_error
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse)
+lin_rmse
+
+from sklearn.metrics import mean_absolute_error
+lin_mae = mean_absolute_error(housing_labels, housing_predictions)
+lin_mae
+
+from sklearn.tree import DecisionTreeRegressor
+
+tree_reg = DecisionTreeRegressor(random_state=42)
+tree_reg.fit(housing_prepared, housing_labels)
+housing_predictions = tree_reg.predict(housing_prepared)
+tree_mse = mean_squared_error(housing_labels, housing_predictions)
+tree_rmse = np.sqrt(tree_mse)
+tree_rmse
+
+
+"""Better evaluation using Crpss Validation"""
+
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
+                         scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
+                         scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+
+lin_scores = cross_val_score(lin_reg, housing_prepared, housing_labels,
+                             scoring="neg_mean_squared_error", cv=10)
+lin_rmse_scores = np.sqrt(-lin_scores)
+display_scores(lin_rmse_scores)
+
+from sklearn.ensemble import RandomForestRegressor
+
+forest_reg = RandomForestRegressor(n_estimators=100, random_state=42)
+forest_reg.fit(housing_prepared, housing_labels)
+housing_predictions = forest_reg.predict(housing_prepared)
+forest_mse = mean_squared_error(housing_labels, housing_predictions)
+forest_rmse = np.sqrt(forest_mse)
+forest_rmse
+
+from sklearn.model_selection import cross_val_score
+
+forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+forest_rmse_scores = np.sqrt(-forest_scores)
+display_scores(forest_rmse_scores)
+
+scores = cross_val_score(lin_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+pd.Series(np.sqrt(-scores)).describe()
+
+from sklearn.svm import SVR
+svm_reg = SVR(kernel="linear")
+svm_reg.fit(housing_prepared, housing_labels)
+housing_predictions = svm_reg.predict(housing_prepared)
+svm_mse = mean_squared_error(housing_labels, housing_predictions)
+svm_rmse = np.sqrt(svm_mse)
+svm_rmse
+svm_scores = cross_val_score(svm_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+pd.Series(np.sqrt(-svm_scores)).describe()
+svm_rmse_scores = np.sqrt(-svm_scores)
+
+
+from sklearn.model_selection import cross_val_score
+
+forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+forest_rmse_scores = np.sqrt(-forest_scores)
+display_scores(forest_rmse_scores)
+
+scores = cross_val_score(lin_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+pd.Series(np.sqrt(-scores)).describe()
+
+from sklearn.svm import SVR
+svm_reg = SVR(kernel="linear")
+svm_reg.fit(housing_prepared, housing_labels)
+housing_predictions = svm_reg.predict(housing_prepared)
+svm_mse = mean_squared_error(housing_labels, housing_predictions)
+svm_rmse = np.sqrt(svm_mse)
+svm_rmse
+svm_scores = cross_val_score(svm_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+pd.Series(np.sqrt(-svm_scores)).describe()
+svm_rmse_scores = np.sqrt(-svm_scores)
+
+
+"""Fine tune your model - gridsearchcv"""
+
+from sklearn.model_selection import GridSearchCV
+
+param_grid = [
+    # try 12 (3×4) combinations of hyperparameters
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    # then try 6 (2×3) combinations with bootstrap set as False
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+  ]
+
+forest_reg = RandomForestRegressor(random_state=42)
+# train across 5 folds, that's a total of (12+6)*5=90 rounds of training
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                           scoring='neg_mean_squared_error',
+                           return_train_score=True)
+grid_search.fit(housing_prepared, housing_labels)
+
+print (grid_search.best_params_)
+print(grid_search.best_estimator_)
+
+"""Let's look at the score of each hyperparameter combination tested during the grid search:"""
+cvres = grid_search.cv_results_
+for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+    print(np.sqrt(-mean_score), params
+    pd.DataFrame(grid_search.cv_results_)
+
+
