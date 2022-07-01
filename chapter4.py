9476416fe9da34312a2e6d8a8252f48e2a0a8429
kunalgarg <gargkunal@outlook.com>
2022-07-01 23:04:51 +0530

    commiting chapter 4

---
+++ chapter4.py
@@ -0,0 +1,22 @@
+"""
+A. Linear regression
+
+1. Linear equation
+    a. using normal equation
+    b. using sklearn.linear_model - linear regression - SVD method
+2. Gradient descent
+    a. batch gradient descent - learning rate picking, tolerance level to exit
+    b. stochastic gradient descent - rearning rate schedule setting
+3. SGD regression - linear regression using stochastic  gd / mini batch gradient descent / Batch
+4. Mini batch gradient descent
+
+B. Polynomial regression
+1. using sklearn.preprocessing to convert linear features to polynomial features
+2. Learning curves
+3. Learning curve - Ways to detect underfitting and overfitting,
+4. Bias variance tradeoff
+
+
+C.  Regularized Linear model
+
+"""
